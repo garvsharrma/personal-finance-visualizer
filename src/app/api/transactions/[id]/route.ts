1b58c7d48176847bec0f4f9d71f2716a8e7647b2
Garv Sharma <133889481+garvsharrma@users.noreply.github.com>
2025-04-16 12:20:11 +0530

    Update route.ts

--- src/app/api/transactions/[id]/route.ts
+++ src/app/api/transactions/[id]/route.ts
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 
 export async function DELETE(
   request: NextRequest,
-  { params }: { params: Record<string, string> }
+  { params }: { params: Promise<{ id : string}> }
 ) {
   try {
     const id = params.id;
